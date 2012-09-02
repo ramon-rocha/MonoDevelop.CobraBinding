2ca64525d60dd45839faadea850d8ecb34c4fd4d
Ramon Rocha <ra.rocha.jr@gmail.com>
2012-09-01 19:43:58 -0500

    Add new C# project for user interfaces
    Includes interface that will ultimately be used for compiler
    options.  Not connected yet to a backend yet and CobraBinding
    project does not reference this new project yet either.

---
+++ Gui/CompilerBuildOptionsPanelWidget.cs
@@ -0,0 +1,13 @@
+using System;
+namespace MonoDevelop.CobraBinding.Gui
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class CompilerBuildOptionsPanelWidget : Gtk.Bin
+	{
+		public CompilerBuildOptionsPanelWidget ()
+		{
+			this.Build ();
+		}
+	}
+}
+
